eeddf711a99e06327df646349f9a747d672b148d
Vu Le <leducvuvietnam@gmail.com>
2024-04-23 04:58:09 +0000

    changed contents for MarkovAI landing page

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'React landing page',
-  title: 'React landing page template 2021',
+  site_name: 'MarkovAI landing page',
+  title: 'MarkovAI landing page 2024',
   description: 'Production ready plug n play landing page!',
   locale: 'en',
 };
