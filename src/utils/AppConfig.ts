bb73c093ffa8438f08aa5b32431b26210c3a2672
Anthony Le <leducvuvietnam@gmail.com>
2024-06-22 22:24:26 +0700

    added new landing page template, dark version, and more attractive

--- src/utils/AppConfig.ts
+++
@@ -1,6 +0,0 @@
-export const AppConfig = {
-  site_name: 'MarkovAI landing page',
-  title: 'MarkovAI landing page 2024',
-  description: 'Production ready plug n play landing page!',
-  locale: 'en',
-};
